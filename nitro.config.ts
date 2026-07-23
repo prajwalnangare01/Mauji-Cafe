00d934b0aef695836bacfcff936fedfe441c74a4
prajwalnangare01 <prajwalnagare123@gmail.com>
2026-07-23 16:10:46 +0530

    Bind TanStack Start server handler and client assets in Nitro config for Vercel

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,4 +2,16 @@ import { defineNitroConfig } from "nitro/config";
 
 export default defineNitroConfig({
   preset: "vercel",
+  publicAssets: [
+    {
+      dir: "./dist/client",
+      maxAge: 3600,
+    },
+  ],
+  handlers: [
+    {
+      route: "/**",
+      handler: "./dist/server/server.js",
+    },
+  ],
 });
